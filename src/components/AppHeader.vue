495f9ea1b9e4c20aaa6e083c307fdc3fb1d7f41a
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-05 14:15:26 +0100

    Aggiunge array header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,75 @@
 <script>
+import AppLogo from "./AppLogo.vue";
 export default {
   name: 'AppHeader',
+  components: {
+    AppLogo,
+  },
+  data() {
+    return {
+      links: [
+        {
+          href: "#",
+          label: "Home",
+          info: 'face and body',
+        },
+        {
+          href: "#",
+          label: "Prodotti",
+          info: 'face and body',
+        },
+        {
+          href: "#",
+          label: "Chi Siamo",
+          info: 'face and body',
+        },       
+      ],
+    };
+  },
 }
 </script>
 
 <template>
-  <h1>Header</h1>
+  <header class="container">
+    
+    <nav class="main-nav">
+      <ul>
+        
+        <li v-for="(item, index) in links">
+          <i class="fa-solid fa-house-chimney"></i>
+          <a :href="item.href">{{ item.label }}</a>
+          <small>{{item.info}}</small>
+        </li>
+      </ul>
+      <AppLogo />
+      <ul>
+        <li v-for="(item, index) in links">
+          <a :href="item.href">{{ item.label }}</a>
+          <small>{{item.info}}</small>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<style>
+<style lang="scss">
+header {
+  text-align: center;
+  margin-bottom: 3.125rem;
+  .main-nav {
+    display: flex;
+    ul {
+      display: flex;
+      list-style: none;
+      justify-content: center;
+      a {
+        display: inline-block;
+        padding: 15px 25px;
+        text-decoration: none;
+        color: #000;
+       
+      }
+    }
+  }
+}
 </style>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "AppLogo",
+  data() {
+    return {
+      logo: "avada-spa-logo-new.png",
+    };
+  },
+  
+};
+</script>
+<template>  
+  <img :src="`/src/assets/imgSpa/${logo}`" alt="Logo" />
+</template>
+<style lang="scss">
+    img{
+    height: 25px;
+    width: auto;
+    }
+</style>
